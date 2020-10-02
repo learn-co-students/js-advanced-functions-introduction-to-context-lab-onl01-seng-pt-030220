49d805d3219c0d14c03916e093c57e79403395c0
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-10-01 20:32:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+
+let testEmployee = {};
+
+function createEmployeeRecord(array){
+  testEmployee['firstName'] = array[0];
+  testEmployee['familyName'] = array[1];
+  testEmployee['title'] = array[2];
+  testEmployee['payPerHour'] = array[3];
+  testEmployee['timeInEvents'] = [];
+  testEmployee['timeOutEvents'] = [];
+
+  return testEmployee
+}
+let marcVans = [
+  ['marcos', 'rodriguez', 'tech', 45],
+  ['vanessa', 'rodriguez', 'M.A.', 50]]
+// createEmployeeRecord(marcVans[0])
+
+function createEmployeeRecords(employeesArrays){
+
+  return employeesArrays.map((employee)=>{
+   return createEmployeeRecord(employee)
+  })
+
+}
+
+
+
+console.log(createEmployeeRecords(marcVans))
